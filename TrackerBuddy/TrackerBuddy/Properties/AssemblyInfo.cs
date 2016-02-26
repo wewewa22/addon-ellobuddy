12880f26b46baa842bb1987d0b90cc380211ca84
wewewa22 <wewewa333@gmail.com>
2016-02-26 22:45:22 +0700

    Update AssemblyInfo.cs

--- TrackerBuddy/TrackerBuddy/Properties/AssemblyInfo.cs
+++ TrackerBuddy/TrackerBuddy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TrackerBuddy3")]
+[assembly: AssemblyTitle("TrackerBuddy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrackerBuddy3")]
+[assembly: AssemblyProduct("TrackerBuddy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
