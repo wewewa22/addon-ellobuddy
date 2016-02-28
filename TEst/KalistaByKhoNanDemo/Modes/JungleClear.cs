30bfa34092accad3bd67a468987c70c416ce7de8
wewewa22 <wewewa333@gmail.com>
2016-02-28 21:28:33 +0700

    ID test Kho nAn

---
+++ TEst/KalistaByKhoNanDemo/Modes/JungleClear.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using EloBuddy.SDK;
+using Settings = KalistaByKhoNanDemo.Config.Modes.JungleClear;
+
+namespace KalistaByKhoNanDemo.Modes
+{
+    public class JungleClear : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear);
+        }
+
+        public override void Execute()
+        {
+            if (!Settings.UseE || !E.IsReady())
+            {
+                return;
+            }
+
+            // Get a jungle mob that can die with E
+            if (EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.ServerPosition, E.Range, false).Any(m => m.IsRendKillable()))
+            {
+                E.Cast();
+            }
+        }
+    }
+}
